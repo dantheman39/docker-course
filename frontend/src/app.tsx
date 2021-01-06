758ce668cb4a59bbc72317f8364d4aa65c7330cf
Daniel Riggs <daniel.riggs1@gmail.com>
2021-01-06 09:24:08 -0500

    Install parcel 2 beta to be able to work on linux

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const App = () => {
   return (
     <div>
-      Hi! What's goin on? HMR isn't working with docker-compose, but with docker it does work. Why?
+      Hi! What's goin on? HMR isn't working with docker-compose, but with docker it does work.
+      Works even less well on my linux machine. Boooo. Wait, I figured some of it out
+
     </div>
   );
 };
